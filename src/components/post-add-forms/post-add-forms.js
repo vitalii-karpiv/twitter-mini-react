70f2ed73461cf1e048e776ac7440b37a51f287a8
Vitalii Karpiv <55689318+Vitalik-hash11@users.noreply.github.com>
2020-11-12 22:43:56 +0200

    add onChange listener

--- src/components/post-add-forms/post-add-forms.js
+++ src/components/post-add-forms/post-add-forms.js
@@ -2,23 +2,42 @@ import React from "react";
 
 import "./post-add-form.css";
 
-const PostAddForm = ({ onAdd }) => {
-  return (
-    <div className="bottom-panel">
-      <input
-        className="from-control new-post-label"
-        placeholder="Про що думаєте зараз?"
-        type="text"
-      />
-      <button
-        type="submit"
-        className="btn btn-outline-secondary"
-        onClick={() => onAdd("hello")}
-      >
-        Добавити
-      </button>
-    </div>
-  );
-};
+export default class PostAddForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default PostAddForm;
+    this.state = {
+      inputValue: ""
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  render() {
+    const { onAdd } = this.props;
+
+    return (
+      <div className="bottom-panel">
+        <input
+          className="from-control new-post-label"
+          placeholder="Про що думаєте зараз?"
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.onChange}
+        />
+        <button
+          type="submit"
+          className="btn btn-outline-secondary"
+          onClick={() => {
+            onAdd(this.state.inputValue);
+            this.setState({ inputValue: "" });
+          }}
+        >
+          Добавити
+        </button>
+      </div>
+    );
+  }
+}
